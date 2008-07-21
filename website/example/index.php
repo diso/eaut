ef4deedb8eab393dd6bd91ec7c6eadeb564258b9
wnorris <wnorris@73d59b5e-aa50-0410-a59b-a58dfa41be3d>
2008-07-21 21:19:52 +0000

    show eaut translation process every step of the way... we're effectively redoing everything in the php library, printing debug statements as we go.  It's ugly, but it will work for now.

--- website/example/index.php
+++ website/example/index.php
@@ -12,15 +12,84 @@ require_once 'Auth/Yadis/Email.php';
 $email = $_GET['email'];
 
 if (!empty($email)) {
-	if (isValidEmail($email)) {
-		$url = Auth_Yadis_Email_getID($email);
-		if (empty($url)) {
-			echo '<p>Unable to translate the email address <em>' . $email . '</em></p>';
-		} else {
-			echo '<p>The email address <em>' . $email . '</em> translates to the URL <em>' . $url . '</em></p>';
-		}
-	} else {
+	if (!isValidEmail($email)) {
 		echo '<p>The email address <em>' . $email . '</em> does not appear to be valid.</p>';
+	} else {
+		list($user, $domain) = split('@', $email, 2);
+		$fetcher = Auth_Yadis_Yadis::getHTTPFetcher();
+
+		echo '<ul>';
+
+		echo '<li>Attempting discovery on <em>http://' . $domain . '</em> ... ';
+		$services = Auth_Yadis_Email_getServices('http://' . $domain, $fetcher);
+		echo (empty($services) ? 'failed' : 'success') . '</li>';
+
+		if (empty($services)) {
+			echo '<li>Attempting discovery on <em>http://www.' . $domain . '</em> ... ';
+			$services = Auth_Yadis_Email_getServices('http://www.' . $domain, $fetcher);
+			echo (empty($services) ? 'failed' : 'success') . '</li>';
+
+			if (empty($services)) {
+				echo '<li>Attempting discovery on <em>' . Auth_Yadis_Default_Email_Mapper . '</em> ... ';
+				$services = Auth_Yadis_Email_getServices(Auth_Yadis_Default_Email_Mapper, $fetcher);
+				echo (empty($services) ? 'failed' : 'success') . '</li>';
+			}
+		}
+
+		if (empty($services)) {
+			echo '</ul><p>Unable to discover a valid EAUT service for email address <em>' . $email . '</em></p>';
+		} else {
+			foreach ($services as $s) {
+				$types = $s->getTypes();
+				$uris = $s->getURIs();
+
+				if (empty($types) || empty($uris)) {
+					continue;
+				}
+
+				foreach ($types as $t) {
+					switch ($t) {
+						case Auth_Yadis_EAUT_Template_Type:
+							echo '<li>Found EAUT Template: <em>' . urldecode($uris[0]) . '</em></li>';
+							$id =  str_replace(Auth_Yadis_EAUT_Wildcard_Username, $user, urldecode($uris[0]));
+							echo '<li>Using EAUT Template URL: <em>' . $id . '</em></li>';
+							break 3;
+
+						case Auth_Yadis_EAUT_Mapper_Type:
+							echo '<li>Found EAUT Mapping Service: <em>' . $uris[0] . '</em></li>';
+							$url_parts = parse_url($uris[0]);
+
+							if (empty($url_parts['query'])) {
+								$id = $uris[0] . '?email=' . $email;
+							} else {
+								$id =  $uris[0] . '&email=' . $email;
+							}   
+								
+							if ($site_name) {
+								$id .= "&site_name=$site_name";
+							}   
+
+							echo '<li>Using Mapping Service URL: <em>' . $id . '</em></li>';
+							break 3;
+					}
+
+				}
+			}
+
+			if (empty($id)) {
+				echo '</ul><p>Unable to translate the email address <em>' . $email . '</em></p>';
+			} else {
+				$redirects = getRedirectPath($id);
+				if (sizeof($redirects) > 1) {
+					for($i=1; $i<sizeof($redirects); $i++) {
+						echo '<li>URL <em>' . $redirects[$i-1] . '</em> redirects to the URL <em>' . $redirects[$i] . '</em></li>';
+					}
+				}
+				echo '</ul>';
+	
+				echo '<p>The email address <em>' . $email . '</em> translates to the URL <em>' . $redirects[sizeof($redirects)-1] . '</em></p>';
+			}
+		}
 	}
 }
 ?>
@@ -34,7 +103,35 @@ if (!empty($email)) {
 	</body>
 </html>
 <?php
-function isValidEmail($email){
+function isValidEmail($email) {
 	return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$", $email);
 }
+
+function getRedirectPath($url) {
+    $redirects = array();
+
+    while ($url !== false) {
+		$redirects[] = $url;
+
+        // setup curl connection
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+        $result = curl_exec($ch);
+        if ($result !== false && curl_getinfo($ch, CURLINFO_HTTP_CODE) == '302') {
+			preg_match('/^location: (.+)$/im', $result, $matches);
+			$url = $matches[1];
+		} else {
+			break;
+		}
+
+        // cleanup
+        curl_close($ch);
+    }
+
+	return $redirects;
+}
+
 ?>
