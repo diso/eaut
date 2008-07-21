aa4e0d9f56c067e70d827156338fad3a4b48fea0
Will Norris <will@willnorris.com>
2008-07-21 21:46:07 +0000

    common header and footer.  add google analytics.

---
+++ website/header.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title><?php echo $page_title; ?></title>
+		<link rel="stylesheet" type="text/css" href="/style.css" />
+	</head>
+
+	<body>
+
+		<ul id="menu">
+			<li><a href="/">EAUT</a></li>
+			<li><a href="/specs/1.0/">Specification</a></li>
+			<li><a href="/code/">Code</a></li>
+			<li><a href="/example/">Example</a></li>
+		</ul>
