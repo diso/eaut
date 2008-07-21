9969632396f5b84b4eeb7fa593867d18302e2150
wnorris <wnorris@73d59b5e-aa50-0410-a59b-a58dfa41be3d>
2008-07-21 21:53:38 +0000

    update code page

--- website/code/index.php
+++ website/code/index.php
@@ -13,16 +13,25 @@
 		XRDS discovery) and requires version 2.x.x to function.</p>
 
 		<ul>
-			<li><a href="http://eaut.googlecode.com/files/php-eaut-1.0.zip">PHP EAUT Library</a></li>
+			<li><a href="http://eaut.googlecode.com/svn/code/php/trunk/">PHP EAUT Library</a></li>
 		</ul>
 
 
 		<h2>Python</h2>
 
-		<p></p>
+		<p>This library is built on top of JanRain's excellent <a
+			href="http://openidenabled.com/python-openid/">OpenID library</a> (for
+		XRDS discovery) and requires version 2.x.x to function.</p>
 
 		<ul>
 			<li><a href="http://eaut.googlecode.com/svn/code/python/trunk/eaut.py">Python EAUT Library</a></li>
+		</ul>
+
+
+		<h2>Ruby on Rails</h2>
+
+		<ul>
+			<li><a href="http://eaut.googlecode.com/svn/code/rails/trunk/">Ruby on Rails EAUT Library</a></li>
 		</ul>
 
 <?php
