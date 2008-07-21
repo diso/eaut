e6ecba06aeafb77c68f987964f969f5213e4efe0
wnorris <wnorris@73d59b5e-aa50-0410-a59b-a58dfa41be3d>
2008-07-21 04:02:03 +0000

    fix email validation regex

--- website/example/index.php
+++ website/example/index.php
@@ -35,6 +35,6 @@ if (!empty($email)) {
 </html>
 <?php
 function isValidEmail($email){
-	return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email);
+	return eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$", $email);
 }
 ?>
